03ac0352778560b01f68e7a799982807619e89b0
Riwal CLECH <clech@et.esiea.fr>
2025-04-28 11:46:04 +0200

    Docker update

--- website.py
+++ website.py
@@ -38,4 +38,4 @@ def getScore(username,stones,stars,quartiersStr):
 # lance le serveur web
 print("Start Flask")
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5400)
+    app.run(debug=True, host='0.0.0.0', port=5400)
